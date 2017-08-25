d5b673a3222b18d05005bbad12d4c5224867f7af
Christian Zuluaga <christian.zuluaga247@gmail.com>
2017-08-25 12:07:48 -0500

    Update module4 assignment using forEach

--- module4/script.js
+++ module4/script.js
@@ -41,7 +41,7 @@ WARNING!!! WARNING!!!
   // using the 'speak' method or either helloSpeaker's or byeSpeaker's
   // 'speak' method.
   // See Lecture 50, part 1
-  for (var i in names) {
+  names.forEach( function(name) {
     // STEP 11:
     // Retrieve the first letter of the current name in the loop.
     // Use the string object's 'charAt' function. Since we are looking for
@@ -49,16 +49,16 @@ WARNING!!! WARNING!!!
     // string object's 'toLowerCase' method on the result so we can compare
     // to lower case character 'j' afterwards.
     // Look up these methods on Mozilla Developer Network web site if needed.
-    var firstLetter = names[i].charAt(0).toLowerCase();
+    var firstLetter = name.charAt(0).toLowerCase();
     // STEP 12:
     // Compare the 'firstLetter' retrieved in STEP 11 to lower case
     // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
     // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
     // name in the loop.
     if (firstLetter == 'j'){
-      byeSpeaker.speak(names[i]);
+      byeSpeaker.speak(name);
     } else {
-      helloSpeaker.speak(names[i]);
+      helloSpeaker.speak(name);
     }
-  }
+  });
 })()
